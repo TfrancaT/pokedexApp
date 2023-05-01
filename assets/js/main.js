dc6f20b95ddeaeac5aaea69bcf0551efc2a8fa93
dteslab <talesfranca.97@gmail.com>
2023-05-01 15:49:10 -0300

    separando consumo do html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,3 @@
-const offset = 0;
-const limit = 12;
-
-const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
   return `
@@ -24,15 +20,16 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-  .then((response) => response.json())
-  .then((jsonBody) => jsonBody.results)
-  .then((pokemons) => {
+  pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
+
+
 
     for (let i = 0; i < pokemons.length; i++) {
         const pokemon = pokemons[i];
-        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        listItems.push(convertPokemonToLi(pokemon))
     }
 
+    console.log(listItems)
   })
   .catch((error) => console.error(error))
